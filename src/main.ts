494afff5d368da537e53787d62e12aa6855babfc
xiaozhongwen <zhongwenxiao@hotmail.com>
2023-06-26 06:42:26 +0800

    添加路由

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,32 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import ColumnDetail from './components/ColumnDetail.vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const routerHistory = createWebHashHistory()
+const router = createRouter({
+  history: routerHistory,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/column/:id',
+      name: 'column',
+      component: ColumnDetail
+    }
+  ]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
